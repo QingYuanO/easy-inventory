3c418b97798fb89bef8d05ddab69583a23014f90
qingyuano <211720950@qq.com>
2024-03-02 19:25:49 +0800

    refactor: 提取组件

--- src/app/shop/me/page.tsx
+++ src/app/shop/me/page.tsx
@@ -1,13 +1,13 @@
-import LogoutButton from "@/components/LogoutButton";
-import { buttonVariants } from "@/components/ui/button";
+import SignOutWrap from "@/components/SignOutWrap";
+import { Button, buttonVariants } from "@/components/ui/button";
 import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
 import { getServerAuthSession } from "@/server/auth";
 import { ChevronRight, Smartphone, Store } from "lucide-react";
 import Link from "next/link";
@@ -15,71 +15,76 @@ import React from "react";
 
 export default async function Page() {
   const session = await getServerAuthSession();
-  const user = session?.user;
+  const shop = session?.user;
   return (
     <div className="flex flex-col">
       <div className="space-y-2 border-b border-border p-4">
         <p className="flex items-center gap-2 text-xl font-bold">
           <Store />
-          {user?.name}
+          {shop?.name}
         </p>
         <p className="flex items-center gap-2">
           <Smartphone className="size-4" />
-          {user?.phone}
+          {shop?.phone}
         </p>
       </div>
 
       <div className="mt-4 grid grid-cols-2 gap-4 p-4">
-        <Card className="col-span-1 flex flex-col justify-between">
-          <CardHeader>
-            <CardTitle>创建用户</CardTitle>
-            <CardDescription>为客户创建账号在你的商场下单</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <Link
-              href="/shop/invite-user"
-              className={buttonVariants({ variant: "secondary" })}
-            >
-              立即创建 <ChevronRight />
-            </Link>
-          </CardContent>
-        </Card>
-        <Card className="col-span-1 flex flex-col justify-between">
-          <CardHeader>
-            <CardTitle>我的商品</CardTitle>
-            <CardDescription className="w-full">
-              管理商品，删除、添加等
-            </CardDescription>
-          </CardHeader>
-          <CardContent>
-            <Link
-              href="/shop/goods"
-              className={buttonVariants({ variant: "secondary" })}
-            >
-              立即前往 <ChevronRight />
-            </Link>
-          </CardContent>
-        </Card>
-        <Card className="col-span-1 flex flex-col justify-between">
-          <CardHeader>
-            <CardTitle>用户列表</CardTitle>
-            <CardDescription className="w-full">
-              查看所有用户及其对应的清单
-            </CardDescription>
-          </CardHeader>
-          <CardContent>
-            <Link
-              href="/shop/user-list"
-              className={buttonVariants({ variant: "secondary" })}
-            >
-              立即查看 <ChevronRight />
-            </Link>
-          </CardContent>
-        </Card>
+        <CustomCard
+          link="/shop/invite-user"
+          title="创建用户"
+          desc="为客户创建账号在你的商场下单"
+          btnText="立即创建"
+          className="col-span-1"
+        />
+        <CustomCard
+          link="/shop/goods"
+          title="我的商品"
+          desc="管理商品，删除、添加等"
+          btnText="立即前往"
+          className="col-span-1"
+        />
+        <CustomCard
+          link="/shop/user-list"
+          title="用户列表"
+          desc="查看所有用户及其对应的清单"
+          btnText="立即查看"
+          className="col-span-1"
+        />
       </div>
-      <div className="mt-4 flex justify-center p-4 fixed bottom-16 left-0 w-full">
-        <LogoutButton />
+      <div className="fixed bottom-16 left-0  flex w-full justify-center ">
+        <SignOutWrap>
+          <Button
+            variant="link"
+            className={cn("w-32 text-sm text-muted-foreground")}
+          >
+            退出登录
+          </Button>
+        </SignOutWrap>
       </div>
     </div>
   );
 }
+
+const CustomCard = (props: {
+  link: string;
+  title: string;
+  desc: string;
+  btnText: string;
+  className?: string;
+}) => {
+  const { link, title, desc, className, btnText } = props;
+  return (
+    <Card className={cn("col-span-1 flex flex-col justify-between", className)}>
+      <CardHeader>
+        <CardTitle>{title}</CardTitle>
+        <CardDescription className="w-full">{desc}</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <Link href={link} className={buttonVariants({ variant: "secondary" })}>
+          {btnText} <ChevronRight className="size-4 text-foreground" />
+        </Link>
+      </CardContent>
+    </Card>
+  );
+};
